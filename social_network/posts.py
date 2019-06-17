0d638d6fa10b60a39aab460d3f51c33ea4bf9724
Forrest <11200528+forrestm@users.noreply.github.com>
2019-06-17 16:52:38 -0500

    Updating posts.py

--- social_network/posts.py
+++ social_network/posts.py
@@ -4,33 +4,38 @@ from datetime import datetime
 # create these classes as it corresponds:
 # (your tests will fail if you don't comment out these classes)
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None, user=None):
+        self.text = text
+        self.timestamp = timestamp
+        self.user = user
+
+    def set_user(self, user=None):
+        self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):  # Inherit properly
+    def __init__(self, text, timestamp=None):
+        super().__init__(text, timestamp)
+
+    def __str__(self):
+        return f'@{self.user.first_name} {self.user.last_name}: "{self.text}"\n\t{self.timestamp.strftime("%A, %b %d, %Y")}'
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        super().__init__(text, timestamp)
+        self.image_url = image_url
+
+    def __str__(self):
+        return f'@{self.user.first_name} {self.user.last_name}: "{self.text}"\n\t{self.image_url}\n\t{self.timestamp.strftime("%A, %b %d, %Y")}'
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super().__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        return f'@{self.user.first_name} Checked In: "{self.text}"\n\t{self.latitude}, {self.longitude}\n\t{self.timestamp.strftime("%A, %b %d, %Y")}'

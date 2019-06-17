10ef9d4806ee253fe50ed30f7236623de600bae8
Forrest <11200528+forrestm@users.noreply.github.com>
2019-06-17 17:05:11 -0500

    Updating to work with python2 and early 3

--- social_network/posts.py
+++ social_network/posts.py
@@ -16,26 +16,26 @@ class Post(object):
 
 class TextPost(Post):  # Inherit properly
     def __init__(self, text, timestamp=None):
-        super().__init__(text, timestamp)
+        super(TextPost,self).__init__(text, timestamp)
 
     def __str__(self):
-        return f'@{self.user.first_name} {self.user.last_name}: "{self.text}"\n\t{self.timestamp.strftime("%A, %b %d, %Y")}'
+        return '@{} {}: "{}"\n\t{}'.format(self.user.first_name,self.user.last_name,self.text,self.timestamp.strftime("%A, %b %d, %Y"))
 
 
 class PicturePost(Post):  # Inherit properly
     def __init__(self, text, image_url, timestamp=None):
-        super().__init__(text, timestamp)
+        super(PicturePost,self).__init__(text, timestamp)
         self.image_url = image_url
 
     def __str__(self):
-        return f'@{self.user.first_name} {self.user.last_name}: "{self.text}"\n\t{self.image_url}\n\t{self.timestamp.strftime("%A, %b %d, %Y")}'
+        return '@{} {}: "{}"\n\t{}\n\t{}'.format(self.user.first_name,self.user.last_name,self.text,self.image_url,self.timestamp.strftime("%A, %b %d, %Y"))
 
 
 class CheckInPost(Post):  # Inherit properly
     def __init__(self, text, latitude, longitude, timestamp=None):
-        super().__init__(text, timestamp)
+        super(CheckInPost,self).__init__(text, timestamp)
         self.latitude = latitude
         self.longitude = longitude
 
     def __str__(self):
-        return f'@{self.user.first_name} Checked In: "{self.text}"\n\t{self.latitude}, {self.longitude}\n\t{self.timestamp.strftime("%A, %b %d, %Y")}'
+        return '@{} Checked In: "{}"\n\t{}, {}\n\t{}'.format(self.user.first_name,self.text,self.latitude,self.longitude,self.timestamp.strftime("%A, %b %d, %Y"))
